b9a51ee0e775062b13698b8cbc94f42cb252a94d
babayl0729 <babayl0729@gmail.com>
2024-01-30 01:12:20 -0700

    reading files

---
+++ fileIO/open_read.py
@@ -0,0 +1,7 @@
+# Reading Files
+# you can read a file with the open function
+# open returns a file object to you
+# You can read a file object with the read method
+
+file = open("story.txt")
+file.read()
