f2bc6e3f3a0d16ebcfb32225be5ca336caccfc66
babayl0729 <babayl0729@gmail.com>
2024-05-30 19:51:30 -0600

    Accessing list

---
+++ list/list.py
@@ -0,0 +1,27 @@
+# Accessing Values in a List
+
+friends = ["Ashley","Matt","Michael"]
+print(friends[0])
+print(friends[1])
+print(friends[2])
+
+# len is leght
+print(len(friends))
+
+# number to index backwards
+print(friends[-1]) # Michael
+print(friends[-3]) # Ashley
+#print(friends[-4]) # IndexError
+
+# Check if a value is in a List
+"Ashley" in  friends # True
+"Larry" in friends # False
+
+# Iterating over Lists (for and while loops)
+for friend in friends:
+    print(friend)
+
+i = 0
+while i < len(friends):
+    print(friends[i])
+    i+=1
